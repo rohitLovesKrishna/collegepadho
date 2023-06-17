3a9a1e3a5047737e7e5580dd6c806b3e510e8eda
Rohit <ambawata.11@gmail.com>
2023-06-17 10:35:04 +0530

    Skeloton fixed

--- src/components/Global/Skel.js
+++ src/components/Global/Skel.js
@@ -4,9 +4,9 @@ import { Grid } from '@mui/material'
 
 export default function Skel() {
     return (
-        <Grid container spacing={5} mt={"20px"}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1,].map((_) => {
-                return <Grid item lg={5}>
+        <Grid container spacing={5} mt={"20px"} sx={{ p: { xs: "20px" } }}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1,].map((_, index) => {
+                return <Grid key={index} item lg={5} md={5} sm={6} xs={12}>
                     <Skeleton variant="rectangular" width={"100%"} height={180} />
                     <Skeleton variant="rectangular" width={"100%"} height={40} />
                     <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
